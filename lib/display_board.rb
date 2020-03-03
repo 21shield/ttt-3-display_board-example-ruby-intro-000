ee8ef203c9bccead9fa819ff4b84adadf607f17e
Netaly Ramirez <21netaly@gmail.com>
2020-03-03 19:36:38 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Boa
 def display_board
  cell = '   |   |   '
-divider = "------------"
+divider = "-----------"
   puts cell
   puts divider
   puts cell
