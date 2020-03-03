d5364fa31d8e92719c64568e7c94cf69ad65f965
Netaly Ramirez <21netaly@gmail.com>
2020-03-03 19:21:08 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,9 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Boa
 def display_board
   print ' | | '
-  puts " | | "
-  puts " | | "
+  print------------
+  print " | | "
+  print------------
+  print " | | "
+
 end
